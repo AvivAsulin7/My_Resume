b4ad189e7c340d5061395395a27f1701977cd502
אביב אסולין <avivasulin1211@gmail.com>
2022-10-10 12:49:32 +0300

    second

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,37 @@
 import React from "react";
 import "./About.css";
+import London_style2 from "../../assets/London_style2.png";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={London_style2} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque eos
+            voluptatem eius dolorem maiores nihil ducimus at rem ullam
+            reprehenderit quidem quia deserunt, molestiae, eligendi amet
+            repellat molestias quos totam.
+          </p>
+
+          <a href="#contact" className="btn" id="btn1">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { AiOutlineHome } from "react-icons/ai";
 import { AiOutlineUser } from "react-icons/ai";
@@ -7,21 +7,42 @@ import { MdWorkOutline } from "react-icons/md";
 import { AiOutlineMessage } from "react-icons/ai";
 
 const Navbar = () => {
+  const [activeNav, setActiveNav] = useState("#");
   return (
     <nav>
-      <a href="#">
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
         <AiOutlineHome />
       </a>
-      <a href="#about">
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
         <AiOutlineUser />
       </a>
-      <a href="#skills">
+      <a
+        href="#skills"
+        onClick={() => setActiveNav("#skills")}
+        className={activeNav === "#skills" ? "active" : ""}
+      >
         <SiSkillshare />
       </a>
-      <a href="#projects">
+      <a
+        href="#projects"
+        onClick={() => setActiveNav("#projects")}
+        className={activeNav === "#projects" ? "active" : ""}
+      >
         <MdWorkOutline />
       </a>
-      <a href="#contact">
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
         <AiOutlineMessage />
       </a>
     </nav>
